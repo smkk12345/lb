fddbe56381c937896257243a4574972bba966312
yinxc <yinxc@longbei.com>
2017-02-08 17:44:46 +0800

    消息

--- src/main/java/com/longbei/appservice/dao/UserMsgMapper.java
+++ src/main/java/com/longbei/appservice/dao/UserMsgMapper.java
@@ -1,17 +1,69 @@
 package com.longbei.appservice.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.longbei.appservice.entity.UserMsg;
 
 public interface UserMsgMapper {
     int deleteByPrimaryKey(Integer id);
+    
+    /**
+	 * @author yinxc
+	 * 删除消息(假删)
+	 * 2017年2月7日
+	 * mtype 0 系统消息(通知消息.进步消息等) 1 对话消息(msgtype 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等)
+	 * return_type
+	 * UserMsgService
+	 */
+    int deleteByUserid(@Param("userid") long userid, @Param("mtype") String mtype);
 
     int insert(UserMsg record);
 
     int insertSelective(UserMsg record);
 
     UserMsg selectByPrimaryKey(Integer id);
+    
+    /**
+	 * @author yinxc
+	 * 获取消息信息
+	 * 2017年2月7日
+	 * return_type
+	 * UserMsgService
+	 */
+//	List<UserMsg> selectByGtypeAndSnsid(@Param("gtype") String gtype, @Param("snsid") Long snsid);
+    
+    /**
+	 * @author yinxc
+	 * 获取消息列表信息(系统消息)
+	 * 2017年2月7日
+	 * return_type
+	 * UserMsgService
+	 */
+    List<UserMsg> selectByUserid(@Param("userid") long userid, @Param("startNum") int startNum, @Param("endNum") int endNum);
 
     int updateByPrimaryKeySelective(UserMsg record);
 
     int updateByPrimaryKey(UserMsg record);
+    
+    /**
+	 * @author yinxc
+	 * 修改消息已读状态信息
+	 * 2017年2月7日
+	 * return_type
+	 * UserMsgService
+	 */
+    int updateIsreadByid(@Param("id") Integer id);
+    
+    /**
+	 * @author yinxc
+	 * 修改消息已读状态信息
+	 * 2017年2月7日
+	 * mtype 0 系统消息(通知消息.进步消息等) 1 对话消息(msgtype 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等)
+	 * return_type
+	 * UserMsgService
+	 */
+    int updateIsreadByUserid(@Param("userid") long userid, @Param("mtype") String mtype);
+    
 }
--- src/main/java/com/longbei/appservice/entity/UserMsg.java
+++ src/main/java/com/longbei/appservice/entity/UserMsg.java
@@ -2,166 +2,214 @@ package com.longbei.appservice.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class UserMsg {
-    private Integer id;
+	private Integer id;
 
-    private Long userid;
+	private Long userid;
 
-    private Long friendid;
+	private Long friendid;
 
-    private String msgtype;//0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
+	private String msgtype;// 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
 
-    private Long snsid;//业务id
+	private Long snsid;// 业务id
 
-    private String remark;//可能会有一些说明
+	private String remark;// 可能会有一些说明
 
-    private String isdel;//消息假删  0 未删 1 假删
+	private String isdel;// 消息假删 0 未删 1 假删
 
-    private String isread;//0 未读  1 已读
+	private String isread;// 0 未读 1 已读
 
-    private Date createtime;
+	private String gtype; // 0 零散 1 目标中 2 榜中 3圈子中 4 教室中
 
-    /**
-     * 
-     * @return id 
-     */
-    public Integer getId() {
-        return id;
-    }
+	private String mtype; // 0 系统消息(通知消息.进步消息等) 1 对话消息(msgtype 0 聊天 1 评论 2 点赞 3
+							// 送花 4 送钻石 等等)
 
-    /**
-     * 
-     * @param id 
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	private Date createtime;
 
-    /**
-     * 
-     * @return userid 
-     */
-    public Long getUserid() {
-        return userid;
-    }
+	/**
+	 * 
+	 * @return id
+	 */
+	public Integer getId() {
+		return id;
+	}
 
-    /**
-     * 
-     * @param userid 
-     */
-    public void setUserid(Long userid) {
-        this.userid = userid;
-    }
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    /**
-     * 
-     * @return friendid 
-     */
-    public Long getFriendid() {
-        return friendid;
-    }
+	/**
+	 * 
+	 * @return userid
+	 */
+	@JsonInclude(Include.ALWAYS)
+	public Long getUserid() {
+		return userid;
+	}
 
-    /**
-     * 
-     * @param friendid 
-     */
-    public void setFriendid(Long friendid) {
-        this.friendid = friendid;
-    }
+	/**
+	 * 
+	 * @param userid
+	 */
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
 
-    /**
-     * 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
-     * @return msgtype 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
-     */
-    public String getMsgtype() {
-        return msgtype;
-    }
+	/**
+	 * 
+	 * @return friendid
+	 */
+	@JsonInclude(Include.ALWAYS)
+	public Long getFriendid() {
+		return friendid;
+	}
 
-    /**
-     * 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
-     * @param msgtype 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
-     */
-    public void setMsgtype(String msgtype) {
-        this.msgtype = msgtype == null ? null : msgtype.trim();
-    }
+	/**
+	 * 
+	 * @param friendid
+	 */
+	public void setFriendid(Long friendid) {
+		this.friendid = friendid;
+	}
 
-    /**
-     * 业务id
-     * @return snsid 业务id
-     */
-    public Long getSnsid() {
-        return snsid;
-    }
+	/**
+	 * 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
+	 * 
+	 * @return msgtype 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
+	 */
+	@JsonInclude(Include.ALWAYS)
+	public String getMsgtype() {
+		return msgtype;
+	}
 
-    /**
-     * 业务id
-     * @param snsid 业务id
-     */
-    public void setSnsid(Long snsid) {
-        this.snsid = snsid;
-    }
+	/**
+	 * 0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
+	 * 
+	 * @param msgtype
+	 *            0 聊天 1 评论 2 点赞 3 送花 4 送钻石 等等
+	 */
+	public void setMsgtype(String msgtype) {
+		this.msgtype = msgtype == null ? null : msgtype.trim();
+	}
 
-    /**
-     * 可能会有一些说明
-     * @return remark 可能会有一些说明
-     */
-    public String getRemark() {
-        return remark;
-    }
+	/**
+	 * 业务id
+	 * 
+	 * @return snsid 业务id
+	 */
+	@JsonInclude(Include.ALWAYS)
+	public Long getSnsid() {
+		return snsid;
+	}
 
-    /**
-     * 可能会有一些说明
-     * @param remark 可能会有一些说明
-     */
-    public void setRemark(String remark) {
-        this.remark = remark == null ? null : remark.trim();
-    }
+	/**
+	 * 业务id
+	 * 
+	 * @param snsid
+	 *            业务id
+	 */
+	public void setSnsid(Long snsid) {
+		this.snsid = snsid;
+	}
 
-    /**
-     * 消息假删  0 未删 1 假删
-     * @return isdel 消息假删  0 未删 1 假删
-     */
-    public String getIsdel() {
-        return isdel;
-    }
+	/**
+	 * 可能会有一些说明
+	 * 
+	 * @return remark 可能会有一些说明
+	 */
+	public String getRemark() {
+		return remark;
+	}
 
-    /**
-     * 消息假删  0 未删 1 假删
-     * @param isdel 消息假删  0 未删 1 假删
-     */
-    public void setIsdel(String isdel) {
-        this.isdel = isdel == null ? null : isdel.trim();
-    }
+	/**
+	 * 可能会有一些说明
+	 * 
+	 * @param remark
+	 *            可能会有一些说明
+	 */
+	public void setRemark(String remark) {
+		this.remark = remark == null ? null : remark.trim();
+	}
 
-    /**
-     * 0 未读  1 已读
-     * @return isread 0 未读  1 已读
-     */
-    public String getIsread() {
-        return isread;
-    }
+	/**
+	 * 消息假删 0 未删 1 假删
+	 * 
+	 * @return isdel 消息假删 0 未删 1 假删
+	 */
+	@JsonInclude(Include.ALWAYS)
+	public String getIsdel() {
+		return isdel;
+	}
 
-    /**
-     * 0 未读  1 已读
-     * @param isread 0 未读  1 已读
-     */
-    public void setIsread(String isread) {
-        this.isread = isread == null ? null : isread.trim();
-    }
+	/**
+	 * 消息假删 0 未删 1 假删
+	 * 
+	 * @param isdel
+	 *            消息假删 0 未删 1 假删
+	 */
+	public void setIsdel(String isdel) {
+		this.isdel = isdel == null ? null : isdel.trim();
+	}
 
-    /**
-     * 
-     * @return createtime 
-     */
-    public Date getCreatetime() {
-        return createtime;
-    }
+	/**
+	 * 0 未读 1 已读
+	 * 
+	 * @return isread 0 未读 1 已读
+	 */
+	@JsonInclude(Include.ALWAYS)
+	public String getIsread() {
+		return isread;
+	}
 
-    /**
-     * 
-     * @param createtime 
-     */
-    public void setCreatetime(Date createtime) {
-        this.createtime = createtime;
-    }
+	/**
+	 * 0 未读 1 已读
+	 * 
+	 * @param isread
+	 *            0 未读 1 已读
+	 */
+	public void setIsread(String isread) {
+		this.isread = isread == null ? null : isread.trim();
+	}
+
+	/**
+	 * 
+	 * @return createtime
+	 */
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	/**
+	 * 
+	 * @param createtime
+	 */
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	@JsonInclude(Include.ALWAYS)
+	public String getGtype() {
+		return gtype;
+	}
+
+	public void setGtype(String gtype) {
+		this.gtype = gtype;
+	}
+
+	@JsonInclude(Include.ALWAYS)
+	public String getMtype() {
+		return mtype;
+	}
+
+	public void setMtype(String mtype) {
+		this.mtype = mtype;
+	}
+	
 }
