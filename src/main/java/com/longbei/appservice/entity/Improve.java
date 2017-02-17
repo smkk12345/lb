ae8634c78fafed31e7d6193b9d1cac2f98a75511
lixb <lixb@longbei.com>
2017-02-17 18:39:06 +0800

    no message

--- src/main/java/com/longbei/appservice/entity/Improve.java
+++ src/main/java/com/longbei/appservice/entity/Improve.java
@@ -55,17 +55,83 @@ public class Improve {
 
     protected Integer commentnum = 0; //评论数
 
-
+    /**
+     * 以下扩展返回信息
+     * 进步人信息
+     * 是否点过赞送过花 送过钻
+     * 是否已经收藏
+     * 点赞送花送钻用户列表
+     * 点赞送花送钻人数
+     * 关联的超级话题
+     * @return
+     */
     protected AppUserMongoEntity appUserMongoEntity; //进步用户信息
 
-    private ImproveExpandData improveExpandData = new ImproveExpandData();
+    private List<AppUserMongoEntity> appUserList;//点赞送花送钻用户列表 只取前5条
 
-    public void setImproveExpandData(ImproveExpandData improveExpandData) {
-        this.improveExpandData = improveExpandData;
+    private Integer lfdcount = 0;//点赞送花送钻 数量
+
+    private String haslike = "0";//是否点赞
+    private String hasflower = "0";//是否送花
+    private String hasdiamond = "0";//时候送钻
+    private String hascollect = "0";//是否收藏
+
+    private SuperTopic superTopic;//超级话题
+
+    public List<AppUserMongoEntity> getAppUserList() {
+        return appUserList;
     }
 
-    public ImproveExpandData getImproveExpandData() {
-        return improveExpandData;
+    public void setAppUserList(List<AppUserMongoEntity> appUserList) {
+        this.appUserList = appUserList;
+    }
+
+    public Integer getLfdcount() {
+        return lfdcount;
+    }
+
+    public void setLfdcount(Integer lfdcount) {
+        this.lfdcount = lfdcount;
+    }
+
+    public String getHaslike() {
+        return haslike;
+    }
+
+    public void setHaslike(String haslike) {
+        this.haslike = haslike;
+    }
+
+    public String getHasflower() {
+        return hasflower;
+    }
+
+    public void setHasflower(String hasflower) {
+        this.hasflower = hasflower;
+    }
+
+    public String getHasdiamond() {
+        return hasdiamond;
+    }
+
+    public void setHasdiamond(String hasdiamond) {
+        this.hasdiamond = hasdiamond;
+    }
+
+    public String getHascollect() {
+        return hascollect;
+    }
+
+    public void setHascollect(String hascollect) {
+        this.hascollect = hascollect;
+    }
+
+    public SuperTopic getSuperTopic() {
+        return superTopic;
+    }
+
+    public void setSuperTopic(SuperTopic superTopic) {
+        this.superTopic = superTopic;
     }
 
     public AppUserMongoEntity getAppUserMongoEntity() {
