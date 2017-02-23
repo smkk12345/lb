efd2d11bc9dc0394dbc7cce21a960eac2b2c4337
Ye Lu <490165164@qq.com>
2017-02-23 11:30:07 +0800

    榜单操作

--- src/main/java/com/longbei/appservice/controller/api/RankApiController.java
+++ src/main/java/com/longbei/appservice/controller/api/RankApiController.java
@@ -153,53 +153,15 @@ public class RankApiController {
 
     /**
      * 编辑榜单
-     * @param rankid  榜单id
-     * @param rankdetail  榜单详情
-     * @param ranktitle  榜单标题
-     * @param ranklimite 榜单限制人数
-     * @param rankscope  榜单范围
-     * @param rankphotos 榜单图片
-     * @param rankrate  榜单中奖率
-     * @param starttime  开始时间
-     * @param endtime   结束时间
-     * @param areaname  区域名字
-     * @param createuserid 创建人id
-     * @param ranktype  榜单类型
-     * @param ispublic  是否公开
-     * @param rankcateid 榜单类型
-     * @param likescore 赞分数
-     * @param flowerscore 花分数
-     * @param diamondscore 钻石分数
-     * @param codeword 入榜口令
-     * @param ptype 十全十美类型
-     * @param sourcetype 来源类型
-     * @param companyname  公司名字
-     * @param companyphotos 公司图片
-     * @param companybrief 公司简介
      * @return
      */
     @ResponseBody
-    @RequestMapping(value = "update")
-    public BaseResp<Object> updateRank(String rankid,String rankdetail,String ranktitle,
-                                       String ranklimite, String rankscope,
-                                       String rankphotos, String rankrate,
-                                       String starttime,String endtime,String areaname,
-                                       String createuserid,String ranktype,String ispublic,
-                                       String rankcateid,String likescore,
-                                       String flowerscore,String diamondscore,
-                                       String codeword,String ptype,String sourcetype,
-                                       String companyname,String companyphotos,
-                                       String companybrief){
+    @RequestMapping(value = "updateimage")
+    public BaseResp<Object> updateRankImage(@RequestBody RankImage rankImage){
 
         boolean issuccess = false;
         try {
-            issuccess = rankService.updateRank(Long.parseLong(rankid),rankdetail,ranktitle,
-                    Integer.parseInt(ranklimite),rankscope,rankphotos,Double.parseDouble(rankrate),
-                    DateUtils.formatDate(starttime,"yyyy-MM-dd HH:mm:ss"),
-                    DateUtils.formatDate(endtime,"yyyy-MM-dd HH:mm:ss"),areaname,
-                    createuserid,ranktype,ispublic,rankcateid,Integer.parseInt(likescore),
-                    Integer.parseInt(flowerscore),Integer.parseInt(diamondscore),codeword,ptype,
-                    sourcetype,companyname,companyphotos,companybrief);
+            issuccess = rankService.updateRankImage(rankImage);
             if(issuccess){
                 return BaseResp.ok(Constant.RTNINFO_SYS_52);
             }
