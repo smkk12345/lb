2abdae6566b5ecc2d76afa98d701925d8dfeac24
wucanying <962933013@qq.com>
2017-02-23 09:51:03 +0800

    教育  工作经历

---
+++ src/main/java/com/longbei/appservice/service/impl/UserJobServiceImpl.java
@@ -0,0 +1,118 @@
+package com.longbei.appservice.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.longbei.appservice.common.BaseResp;
+import com.longbei.appservice.common.constant.Constant;
+import com.longbei.appservice.dao.UserJobMapper;
+import com.longbei.appservice.entity.UserJob;
+import com.longbei.appservice.entity.UserSchool;
+import com.longbei.appservice.service.UserJobService;
+
+@Service("userJobService")
+public class UserJobServiceImpl implements UserJobService {
+
+	@Autowired
+	private UserJobMapper userJobMapper;
+	
+	private static Logger logger = LoggerFactory.getLogger(UserJobServiceImpl.class);
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> insertJob(long userid,String companyname,String department,String location,Date starttime,Date endtime) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		UserJob data = new UserJob();
+		data.setUserid(userid);
+		data.setCompanyname(companyname);
+		data.setDepartment(department);
+		data.setLocation(location);		
+		data.setStarttime(starttime);
+		data.setEndtime(endtime);		
+		Date date = new Date();
+		data.setCreatetime(date);
+		data.setUpdatetime(date);
+		try {
+			int m = userJobMapper.insertJob(data);
+			if(m == 1){
+				baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			 }
+		} catch (Exception e) {
+			logger.error("insertJob error and msg={}",e);
+		}
+		return baseResp;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> deleteJob(int id,long userid) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		try {
+			int m = userJobMapper.deleteJob(id,userid);
+			if(m == 1){
+				baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			 }
+		} catch (Exception e) {
+			logger.error("deleteJob error and msg={}",e);
+		}
+		return baseResp;
+	}
+		
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> selectJobById(int id) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		try {
+			UserJob userJob = userJobMapper.selectJobById(id);
+			baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			baseResp.setData(userJob);	
+		} catch (Exception e) {
+			logger.error("selectJobById error and msg={}",e);
+		}
+		return baseResp;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> selectJobList(long userid) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		try {
+			List<UserJob> list = userJobMapper.selectJobList(userid);
+			baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			baseResp.setData(list);	
+		} catch (Exception e) {
+			logger.error("selectJobList error and msg={}",e);
+		}
+		return baseResp;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> updateJob(int id,String companyname,String department,String location,Date starttime,Date endtime) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		UserJob data = new UserJob();
+		data.setId(id);
+		data.setCompanyname(companyname);
+		data.setDepartment(department);
+		data.setLocation(location);		
+		data.setStarttime(starttime);
+		data.setEndtime(endtime);		
+		Date date = new Date();
+		data.setUpdatetime(date);
+		try {
+			int n = userJobMapper.updateJob(data);
+			if(n == 1){
+				baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			}
+		} catch (Exception e) {
+			logger.error("updateJob error and msg={}",e);
+		}
+		return baseResp;
+	}
+	
+}
---
+++ src/main/java/com/longbei/appservice/service/impl/UserSchoolServiceImpl.java
@@ -0,0 +1,116 @@
+package com.longbei.appservice.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.longbei.appservice.common.BaseResp;
+import com.longbei.appservice.common.constant.Constant;
+import com.longbei.appservice.dao.UserSchoolMapper;
+import com.longbei.appservice.entity.UserSchool;
+import com.longbei.appservice.service.UserSchoolService;
+
+@Service("userSchoolService")
+public class UserSchoolServiceImpl implements UserSchoolService {
+
+	@Autowired
+	private UserSchoolMapper userSchoolMapper;
+	
+	private static Logger logger = LoggerFactory.getLogger(UserSchoolServiceImpl.class);
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> insertSchool(long userid,String schoolname,String Department,Date starttime,Date endtime) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		UserSchool data = new UserSchool();
+		data.setUserid(userid);
+		data.setSchoolname(schoolname);
+		data.setDepartment(Department);
+		data.setStarttime(starttime);
+		data.setEndtime(endtime);		
+		Date date = new Date();
+		data.setCreatime(date);
+		data.setUpdatetime(date);
+		try {
+			int m = userSchoolMapper.insertSchool(data);
+			if(m == 1){
+				baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			 }
+		} catch (Exception e) {
+			logger.error("insertSchool error and msg={}",e);
+		}
+		return baseResp;
+	}
+		
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> deleteSchool(int id,long userid) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		try {
+			int m = userSchoolMapper.deleteSchool(id,userid);
+			if(m == 1){
+				baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			 }
+		} catch (Exception e) {
+			logger.error("deleteSchool error and msg={}",e);
+		}
+		return baseResp;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> selectSchoolById(int id) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		try {
+			UserSchool userSchool = userSchoolMapper.selectSchoolById(id);
+			baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			baseResp.setData(userSchool);	
+		} catch (Exception e) {
+			logger.error("selectSchoolById error and msg={}",e);
+		}
+		return baseResp;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> selectSchoolList(long userid) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		try {
+			List<UserSchool> list = userSchoolMapper.selectSchoolList(userid);
+			baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			baseResp.setData(list);	
+		} catch (Exception e) {
+			logger.error("selectSchoolList error and msg={}",e);
+		}
+		return baseResp;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public BaseResp<Object> updateSchool(int id,String schoolname,String Department,Date starttime,Date endtime) {
+		BaseResp<Object> baseResp = new BaseResp<Object>();
+		UserSchool data = new UserSchool();
+		data.setId(id);
+		data.setSchoolname(schoolname);
+		data.setDepartment(Department);
+		data.setStarttime(starttime);
+		data.setEndtime(endtime);		
+		Date date = new Date();
+		data.setUpdatetime(date);
+		try {
+			int n = userSchoolMapper.updateSchool(data);
+			if(n == 1){
+				baseResp.initCodeAndDesp(Constant.STATUS_SYS_00, Constant.RTNINFO_SYS_00);
+			}
+		} catch (Exception e) {
+			logger.error("updateSchool error and msg={}",e);
+		}
+		return baseResp;
+	}
+	
+}
