495248919507bb98954ac519ff50c4b246080e60
yinxc <yinxc@longbei.com>
2017-03-15 20:33:55 +0800

    app调用product接口

--- src/main/java/com/longbei/appservice/service/api/HttpClient.java
+++ src/main/java/com/longbei/appservice/service/api/HttpClient.java
@@ -4,17 +4,25 @@ import com.longbei.appservice.common.interceptor.ApiRequestInterceptor;
 import com.longbei.appservice.service.api.outernetservice.IAlidayuService;
 import com.longbei.appservice.service.api.outernetservice.IJPushService;
 import com.longbei.appservice.service.api.outernetservice.IRongYunService;
+import com.longbei.appservice.service.api.productservice.IProductBasicService;
 import com.longbei.appservice.service.api.userservice.IUserBasicService;
 import feign.Feign;
 import feign.gson.GsonDecoder;
 
 public class HttpClient {
+	
+	//product_service
+	public static IProductBasicService productBasicService =
+			Feign.builder()
+//			.requestInterceptor(new ApiRequestInterceptor())
+			.decoder(new GsonDecoder())
+            .target(IProductBasicService.class, "http://192.168.1.147:8088/product_service");
 
 	//user_service
 	public static IUserBasicService userBasicService =
 			Feign.builder().requestInterceptor(new ApiRequestInterceptor())
 			.decoder(new GsonDecoder())
-            .target(IUserBasicService.class, "http://192.168.1.105:8081/user_service");
+            .target(IUserBasicService.class, "http://192.168.1.105:8080/user_service");
 	//外网接口 阿里大鱼
 	public static IAlidayuService alidayuService =
 			Feign.builder().requestInterceptor(new ApiRequestInterceptor())
