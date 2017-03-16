f8875409a651435c717bf80f120dff4f1d48b4e8
lixb <lixb@longbei.com>
2017-03-16 15:19:41 +0800

    用户等级特权

--- src/main/java/com/longbei/appservice/controller/AppUserController.java
+++ src/main/java/com/longbei/appservice/controller/AppUserController.java
@@ -1240,4 +1240,24 @@ public class AppUserController extends BaseController {
         return baseResp;
     }
 
+    /**
+     * @Title: http://ip:port/app_service/user/userlevel
+     * @Description: 获取用户龙级信息   grade 用户的龙级
+     * @auther lixb
+     * @currentdate:2017年3月16日
+     */
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/userlevel")
+    @ResponseBody
+    public BaseResp<Object> userlevel(String grade) {
+        BaseResp<Object> baseResp = new BaseResp<Object>();
+        if(StringUtils.hasBlankParams(grade)){
+            return baseResp.initCodeAndDesp(Constant.STATUS_SYS_07,Constant.RTNINFO_SYS_07);
+        }
+        baseResp = userService.userlevel(Integer.parseInt(grade));
+        return baseResp;
+    }
+
+
+
 }
--- src/main/java/com/longbei/appservice/entity/UserLevel.java
+++ src/main/java/com/longbei/appservice/entity/UserLevel.java
@@ -13,6 +13,89 @@ public class UserLevel {
 
     private Double discount;//等级折扣
 
+    private Integer pubrankjoinnum;//公共榜单最大人数
+    private Integer pubranknum;//公共榜单数量
+    private Integer circlenum;//圈子数量
+    private Integer prirankjoinnum;//私有榜单最大人数
+    private Integer priranknum;//私有榜单数量
+    private Integer classroomnum;//教室数量
+    private Integer joinranknum;//能参与榜单的数量
+    private Integer likes;//每天点赞数
+    private Integer updatetime;//更新时间
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setCirclenum(Integer circlenum) {
+        this.circlenum = circlenum;
+    }
+
+    public void setClassroomnum(Integer classroomnum) {
+        this.classroomnum = classroomnum;
+    }
+
+    public void setJoinranknum(Integer joinranknum) {
+        this.joinranknum = joinranknum;
+    }
+
+    public void setPrirankjoinnum(Integer prirankjoinnum) {
+        this.prirankjoinnum = prirankjoinnum;
+    }
+
+    public void setPriranknum(Integer priranknum) {
+        this.priranknum = priranknum;
+    }
+
+    public void setPubrankjoinnum(Integer pubrankjoinnum) {
+        this.pubrankjoinnum = pubrankjoinnum;
+    }
+
+    public void setPubranknum(Integer pubranknum) {
+        this.pubranknum = pubranknum;
+    }
+
+    public void setUpdatetime(Integer updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Integer getCirclenum() {
+        return circlenum;
+    }
+
+    public Integer getClassroomnum() {
+        return classroomnum;
+    }
+
+    public Integer getJoinranknum() {
+        return joinranknum;
+    }
+
+    public Integer getPrirankjoinnum() {
+        return prirankjoinnum;
+    }
+
+    public Integer getPriranknum() {
+        return priranknum;
+    }
+
+    public Integer getPubrankjoinnum() {
+        return pubrankjoinnum;
+    }
+
+    public Integer getPubranknum() {
+        return pubranknum;
+    }
+
+    public Integer getUpdatetime() {
+        return updatetime;
+    }
+
+
     /**
      * 编码
      * @return id 编码
