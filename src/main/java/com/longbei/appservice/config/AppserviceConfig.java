f24ee9be4f82770438bd51d4c8bd1b695a10b66d
lixb <lixb@longbei.com>
2017-03-17 09:49:25 +0800

    配置

--- src/main/java/com/longbei/appservice/config/AppserviceConfig.java
+++ src/main/java/com/longbei/appservice/config/AppserviceConfig.java
@@ -13,34 +13,22 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource({"classpath:appservice.properties"})
 public class AppserviceConfig {
 
-    @Value("${service.user}")
-    private String host_user_service;
-    @Value("${service.outernet}")
-    private String host_outernet_service;
-    @Value("${service.product}")
-    private String host_product_service;
 
+    public static String host_user_service;
+    public static String host_outernet_service;
+    public static String host_product_service;
+
+    @Value("${service.outernet}")
     public void setHost_outernet_service(String host_outernet_service) {
         this.host_outernet_service = host_outernet_service;
     }
-
-    public String getHost_outernet_service() {
-        return host_outernet_service;
-    }
-
-    public String getHost_product_service() {
-        return host_product_service;
-    }
-
-    public String getHost_user_service() {
-        return host_user_service;
-    }
-
+    @Value("${service.product}")
     public void setHost_product_service(String host_product_service) {
         this.host_product_service = host_product_service;
     }
-
+    @Value("${service.user}")
     public void setHost_user_service(String host_user_service) {
         this.host_user_service = host_user_service;
     }
+
 }
