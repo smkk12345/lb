533afb1bc5258666b78a4fba2f79e97f8833ae6a
yinxc <yinxc@longbei.com>
2017-03-20 19:15:09 +0800

    支付宝支付以及回调

---
+++ src/main/java/com/longbei/appservice/service/PayService.java
@@ -0,0 +1,36 @@
+package com.longbei.appservice.service;
+
+import java.util.Map;
+
+import com.longbei.appservice.common.BaseResp;
+
+public interface PayService {
+	
+	/**
+	 * 微信支付
+	 * @author yinxc
+	 * @param @param orderid 订单编号
+	 * @param @param userid 
+	 * 2017年3月20日
+	 */
+	 BaseResp<Object> wxPayMainPage(Long userid, String orderid);
+	 
+	 
+	 /**
+	 * 支付宝支付
+	 * @author yinxc
+	 * @param @param orderid 订单编号
+	 * @param @param userid 
+	 * 2017年3月20日
+	 */
+	 BaseResp<Object> signature(Long userid, String orderid);
+	 
+	 /**
+	 * 支付宝支付回调
+	 * @author yinxc
+	 * @param @param orderType 2：购买龙币
+	 * 2017年3月20日
+	 */
+	 BaseResp<Object> ali(Long userid, String orderType, Map<String, String> resMap);
+
+}
