06c18309f2ec031266c32423955304ad69184be9
lixb <lixb@longbei.com>
2017-03-21 18:22:32 +0800

    H5 注册接口

--- src/main/java/com/longbei/appservice/service/UserService.java
+++ src/main/java/com/longbei/appservice/service/UserService.java
@@ -66,6 +66,14 @@ public interface UserService {
 	* @currentdate:2017年1月16日
 	*/
 	BaseResp<Object> checkSms(String mobile, String random,String deviceindex,String devicetype);
+
+	/**
+	 * 校验验证码
+	 * @param mobile
+	 * @param random
+	 * @return
+	 */
+	BaseResp<Object> checkSms(String mobile, String random);
 	/**
 	* @Title: findPassword
 	* @Description: TODO
