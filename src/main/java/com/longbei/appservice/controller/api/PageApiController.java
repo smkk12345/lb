ee20a272d2701815b3e55fd297693427365aeb4f
Ye Lu <490165164@qq.com>
2017-03-23 09:59:32 +0800

    修改错误

--- src/main/java/com/longbei/appservice/controller/api/PageApiController.java
+++ src/main/java/com/longbei/appservice/controller/api/PageApiController.java
@@ -9,12 +9,10 @@ import com.longbei.appservice.service.PageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.oops.ConstantPoolKlass;
 
 /**
  * 页面管理
