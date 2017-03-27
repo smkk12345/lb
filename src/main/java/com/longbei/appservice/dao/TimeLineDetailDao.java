a155ec12fd5b7decc943b86eadb828db09061624
Ye Lu <490165164@qq.com>
2017-03-27 14:28:23 +0800

    热启动配置

--- src/main/java/com/longbei/appservice/dao/TimeLineDetailDao.java
+++ src/main/java/com/longbei/appservice/dao/TimeLineDetailDao.java
@@ -6,6 +6,7 @@ import com.longbei.appservice.common.dao.BaseMongoDao;
 import com.longbei.appservice.entity.ImproveCircle;
 import com.longbei.appservice.entity.TimeLine;
 import com.longbei.appservice.entity.TimeLineDetail;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -33,17 +34,35 @@ public class TimeLineDetailDao extends BaseMongoDao<TimeLineDetail>{
         mongoTemplate.updateMulti(query,update,TimeLineDetail.class);
     }
 
-    public List<TimeLineDetail> selectImproveList(String isrecommend,String brief,List<String> userids, Date lastdate, int pagesize){
-        Criteria criteria = new Criteria();
+    public List<TimeLineDetail> selectRecommendImproveList(String brief,List<String> userids, Date lastdate,
+                                                  int pagesize){
+        Criteria criteria = Criteria.where("isrecommend").is("1");
         if (null != userids && userids.size() != 0){
             criteria.in(userids);
         }
-
+        if (!StringUtils.isEmpty(brief)){
+            criteria.regex(brief);
+        }
         if (null != lastdate) {
             criteria.and("createdate").lt(lastdate);
         }
-        if ()
+        Query query = new Query(criteria);
+        query.with(new Sort(Sort.Direction.DESC, "recommendtime"));
+        query.limit(pagesize);
+        List<TimeLineDetail> timeLineDetails = mongoTemplate.find(query,TimeLineDetail.class);
+        return timeLineDetails;
     }
+
+    public void updateRecommendImprove(String impid,String businesstype,String isrecommend){
+        Criteria criteria = Criteria.where("impid").is(impid)
+                .and("businesstype").is(businesstype);
+        Query query = new Query(criteria);
+        Update update = new Update();
+        update.set("isrecommend",isrecommend);
+        update.set("recommendtime",new Date());
+        mongoTemplate.updateMulti(query,update, TimeLineDetail.class);
+    }
+
 
     public void deleteImprove(Long improveid,String userid){
         Query query = Query.query(Criteria.where("improveId").is(improveid));
