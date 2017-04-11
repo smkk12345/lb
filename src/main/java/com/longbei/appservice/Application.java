bb9bd2cbebda71d52cbcefdbe6ee88ac454d6ed9
Ye Lu <490165164@qq.com>
2017-04-11 13:16:41 +0800

    no message

--- src/main/java/com/longbei/appservice/Application.java
+++ src/main/java/com/longbei/appservice/Application.java
@@ -8,6 +8,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,6 +20,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties(com.longbei.appservice.config.AppserviceConfig.class)
 @EnableSwagger2
 @ServletComponentScan
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Application extends SpringBootServletInitializer {
     private static Logger logger = Logger.getLogger(Application.class);
 
@@ -24,6 +30,11 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
+    }
+
+    @Bean
+    public RestTemplate getTempate(){
+        return new RestTemplate();
     }
 
 
