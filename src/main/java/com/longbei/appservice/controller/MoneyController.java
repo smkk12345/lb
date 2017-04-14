8bc665e81390ffce010bc88c7bf92bf752bd6fa2
yinxc <yinxc@longbei.com>
2017-04-14 16:33:46 +0800

    no message

--- src/main/java/com/longbei/appservice/controller/MoneyController.java
+++ src/main/java/com/longbei/appservice/controller/MoneyController.java
@@ -3,8 +3,12 @@ package com.longbei.appservice.controller;
 import com.longbei.appservice.common.BaseResp;
 import com.longbei.appservice.common.constant.Constant;
 import com.longbei.appservice.common.utils.StringUtils;
+import com.longbei.appservice.entity.UserFlowerDetail;
+import com.longbei.appservice.service.UserFlowerDetailService;
 import com.longbei.appservice.service.UserImpCoinDetailService;
 import com.longbei.appservice.service.UserMoneyDetailService;
+
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +32,8 @@ public class MoneyController {
 	private UserImpCoinDetailService userImpCoinDetailService;
 	@Autowired
 	private UserMoneyDetailService userMoneyDetailService;
+	@Autowired
+	private UserFlowerDetailService userFlowerDetailService;
 	
 	private static Logger logger = LoggerFactory.getLogger(MoneyController.class);
 	
@@ -115,6 +121,39 @@ public class MoneyController {
   				sSize = pageSize.intValue();
   			}
   			baseResp = userMoneyDetailService.selectListByUserid(Long.parseLong(userid), sNo, sSize);
+        } catch (Exception e) {
+            logger.error("selectMoneyDetail userid = {}, startNo = {}, pageSize = {}", userid, startNo, pageSize, e);
+        }
+  		return baseResp;
+    }
+	
+	
+	/**
+    * @Title: http://ip:port/app_service/money/selectFlowerDetail
+    * @Description: 获取用户收到的鲜花明细
+    * @param @param userid
+    * @param @param startNo   pageSize
+    * @auther yinxc
+    * @currentdate:2017年4月14日
+    */
+	@SuppressWarnings("unchecked")
+ 	@RequestMapping(value = "selectFlowerDetail")
+    public BaseResp<List<UserFlowerDetail>> selectFlowerDetail(String userid, Integer startNo, Integer pageSize) {
+		BaseResp<List<UserFlowerDetail>> baseResp = new BaseResp<>();
+  		if (StringUtils.hasBlankParams(userid)) {
+             return baseResp.initCodeAndDesp(Constant.STATUS_SYS_07, Constant.RTNINFO_SYS_07);
+        }
+  		try {
+  			int sNo = Integer.parseInt(Constant.DEFAULT_START_NO);
+  			int sSize = Integer.parseInt(Constant.DEFAULT_PAGE_SIZE);
+  			if(null != startNo){
+  				sNo = startNo.intValue();
+  			}
+  			if(null != pageSize){
+  				sSize = pageSize.intValue();
+  			}
+  			//origin： 来源  0:龙币兑换;  1:赠与;  2:进步币兑换      3:被赠与
+  			baseResp = userFlowerDetailService.selectListByUseridAndOrigin(Long.parseLong(userid), "3", sNo, sSize);
         } catch (Exception e) {
             logger.error("selectMoneyDetail userid = {}, startNo = {}, pageSize = {}", userid, startNo, pageSize, e);
         }
