5bddfbb92a80b623f52a96679eefd83580eb1cf6
lixb <lixb@longbei.com>
2017-04-15 19:00:23 +0800

    no message

--- src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
@@ -1958,6 +1958,8 @@ public class ImproveServiceImpl implements ImproveService{
         if(StringUtils.hasBlankParams(key,filekey)){
             return baseResp.initCodeAndDesp(Constant.STATUS_SYS_07,Constant.RTNINFO_SYS_07);
         }
+        key = key.replace("longbeimp3/","");
+        key = key.replace("longbeimp4/","");
         String[] filenameArr = key.split("_");
         if(filenameArr.length < 2){
             return baseResp;
