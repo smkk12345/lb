ece92c49a0acd9f3f43fa83e89437f1a11a58929
wangyongzhi <wangyongzhi@longbei.com>
2017-04-24 15:09:18 +0800

    榜单 功能开发

--- src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
@@ -511,8 +511,7 @@ public class ImproveServiceImpl implements ImproveService{
      *  @update 2017/1/23 下午4:54
      */
     @Override
-    public List<Improve> selectRankImproveList(String userid, String rankid,String sift,
-                                               String orderby, int pageNo, int pageSize,String lastdate) {
+    public List<Improve> selectRankImproveList(String userid, String rankid,String sift,String orderby, int pageNo, int pageSize,String lastdate) {
         List<Improve> improves = null;
         try {
             switch (sift){
@@ -1178,7 +1177,7 @@ public class ImproveServiceImpl implements ImproveService{
             //初始化评论数量
             initImproveCommentInfo(improve);
             //初始化进步用户信息
-            initImproveUserInfo(improve,Long.parseLong(userid));
+            initImproveUserInfo(improve,userid != null?Long.parseLong(userid):null);
             //初始化点赞，送花，送钻简略信息
             initLikeFlowerDiamondInfo(improve);
             //初始化是否 点赞 送花 送钻 收藏
@@ -1270,11 +1269,13 @@ public class ImproveServiceImpl implements ImproveService{
      */
     private void initImproveUserInfo(Improve improve,Long userid){
         AppUserMongoEntity appUserMongoEntity = userMongoDao.getAppUser(String.valueOf(improve.getUserid()));
-        //获取好友昵称
-        String remark = userRelationService.selectRemark(userid, improve.getUserid());
         if(null != appUserMongoEntity){
-            if(!StringUtils.isBlank(remark)){
-                appUserMongoEntity.setNickname(remark);
+            if(userid != null){
+                //获取好友昵称
+                String remark = userRelationService.selectRemark(userid, improve.getUserid());
+                if(!StringUtils.isBlank(remark)){
+                    appUserMongoEntity.setNickname(remark);
+                }
             }
             improve.setAppUserMongoEntity(appUserMongoEntity);
         }else{
@@ -1979,6 +1980,13 @@ public class ImproveServiceImpl implements ImproveService{
      * @author luye
      */
     private void initIsOptionForImprove(String userid,Improve improve){
+        if(StringUtils.isEmpty(userid)){
+            improve.setHaslike("0");
+            improve.setHasflower("0");
+            improve.setHasdiamond("0");
+            improve.setHascollect("0");
+            return ;
+        }
 //        ImpAllDetail impAllDetail = new ImpAllDetail();
 //        impAllDetail.setUserid(Long.parseLong(userid));
 //        impAllDetail.setImpid(improve.getImpid());
