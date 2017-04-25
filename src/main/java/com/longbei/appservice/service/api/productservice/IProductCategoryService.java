a836b726ef89a8c0cfd10e14f743c540f58834a0
Ye Lu <490165164@qq.com>
2017-04-25 15:44:02 +0800

    修改错误

--- src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
+++ src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
@@ -7,7 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("productServiceLY")
+@FeignClient("productService")
 @RequestMapping("productService")
 public interface IProductCategoryService {
 	/**
