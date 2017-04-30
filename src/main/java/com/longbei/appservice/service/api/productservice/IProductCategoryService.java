0966595c1d05684b7c0982f4591c28b97396e7f3
wucanyiing <962933013@qq.com>
2017-04-30 19:41:40 +0800

    no message

--- src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
+++ src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
@@ -7,8 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("productServicewcy")
-@RequestMapping("product_service")
+@FeignClient("productService")
+@RequestMapping("productService")
 public interface IProductCategoryService {
 	/**
 	 * @Title: selectCategoryByCateId
