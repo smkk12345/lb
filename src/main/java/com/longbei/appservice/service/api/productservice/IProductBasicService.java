de7a3eaf24964fa72f6d13764bcf326d6ddbe0e1
Ye Lu <490165164@qq.com>
2017-05-02 10:46:08 +0800

    修改路径

--- src/main/java/com/longbei/appservice/service/api/productservice/IProductBasicService.java
+++ src/main/java/com/longbei/appservice/service/api/productservice/IProductBasicService.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("productServiceLY")
+@FeignClient("productService")
 @RequestMapping("productService")
 public interface IProductBasicService {
 
