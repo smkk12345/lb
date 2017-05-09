8876478a917b00d478371c41cb35623b7a70e88c
wucanying <962933013@qq.com>
2017-05-09 19:24:11 +0800

    no message

--- src/main/java/com/longbei/appservice/dao/SysPerfectTagMapper.java
+++ src/main/java/com/longbei/appservice/dao/SysPerfectTagMapper.java
@@ -1,11 +1,14 @@
 package com.longbei.appservice.dao;
 
 import com.longbei.appservice.entity.SysPerfectTag;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface SysPerfectTagMapper {
 
     List<SysPerfectTag> selectRandomTagList();
+
+    List<SysPerfectTag> selectUserTagList(@Param("ptypes")String ptypes []);
 
 }
