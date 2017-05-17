6698c90e46cbcd55c3c408dd6d69ea5998c45135
Ye Lu <490165164@qq.com>
2017-05-17 11:37:59 +0800

    修改bug

--- src/main/java/com/longbei/appservice/service/impl/RankServiceImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/RankServiceImpl.java
@@ -1894,7 +1894,8 @@ public class RankServiceImpl extends BaseServiceImpl implements RankService{
             if(award.getAwardClassify().getClassifytype() == 0){//进步币
                 //加进步币
                 BaseResp<Object> baseResp1 = userImpCoinDetailService.insertPublic(userId,"4",(int) (award.getAwardprice()*Constant.RMB_COIN),rankId,null);
-
+                //更新用户进步币总数
+                userInfoMapper.updateCoinAndFlowerByUserid(userId,(int) (award.getAwardprice()*Constant.RMB_COIN),0);
                 newRankAcceptAward.setPublishawardtype("0");
 
                 if(baseResp1.getCode() == 0){
