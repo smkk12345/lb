81df971a3195308178c2e702f41d1a241e94d4f3
yinxc <yinxc@longbei.com>
2017-05-19 10:15:44 +0800

    no message

--- src/main/java/com/longbei/appservice/service/impl/UserCheckinDetailImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/UserCheckinDetailImpl.java
@@ -106,9 +106,9 @@ public class UserCheckinDetailImpl implements UserCheckinDetailService {
 				String redisvalue = springJedisDao.getHashValue(Constant.RP_USER_CHECK + userid, 
 						Constant.RP_USER_CHECK_VALUE + userid);
 				int cha = Integer.parseInt(day) - Integer.parseInt(redisDate);
-				if(cha == Integer.parseInt(redisvalue) -1){
+				if(cha == Integer.parseInt(redisvalue)){
 					//当天已签到
-					reseResp.initCodeAndDesp(Constant.STATUS_SYS_30,Constant.RTNINFO_SYS_30);
+					reseResp.initCodeAndDesp(Constant.STATUS_SYS_00,Constant.RTNINFO_SYS_30);
 					return reseResp;
 				}else if(cha > Integer.parseInt(redisvalue)){
 					//不是连续签到     先清除    再添加
