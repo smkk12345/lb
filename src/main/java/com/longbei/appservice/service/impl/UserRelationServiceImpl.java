7d9a9903cad5127e351ce9c5a08ca1c00c3d72f2
yinxc <yinxc@longbei.com>
2017-06-02 15:46:00 +0800

    no message

--- src/main/java/com/longbei/appservice/service/impl/UserRelationServiceImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/UserRelationServiceImpl.java
@@ -624,7 +624,7 @@ public class UserRelationServiceImpl implements UserRelationService {
 
 
 	private void insertAddFansMsg(Long userId,Long likeUserId){
-		String remark = "关注消息";
+		String remark = "关注了您";
 		userMsgService.insertMsg(String.valueOf(userId),String.valueOf(likeUserId),
 				null,"15",null,remark,"1","5", "关注",0, "", "");
 
