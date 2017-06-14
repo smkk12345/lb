2d9db17b7badc93d4e7118def6d382273b1dfb12
lichaochao <1510138834@qq.com>
2017-06-14 14:48:17 +0800

    参榜人数

--- src/main/java/com/longbei/appservice/controller/api/RankApiController.java
+++ src/main/java/com/longbei/appservice/controller/api/RankApiController.java
@@ -8,11 +8,9 @@
 */
 package com.longbei.appservice.controller.api;
 
-import com.alibaba.fastjson.JSON;
 import com.longbei.appservice.common.BaseResp;
 import com.longbei.appservice.common.Page;
 import com.longbei.appservice.common.constant.Constant;
-import com.longbei.appservice.common.utils.DateUtils;
 import com.longbei.appservice.common.utils.ResultUtil;
 import com.longbei.appservice.entity.*;
 import com.longbei.appservice.service.ImproveService;
@@ -20,15 +18,12 @@ import com.longbei.appservice.service.RankService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author smkk
@@ -44,7 +39,6 @@ public class RankApiController {
     private  RankService rankService;
     @Autowired
     private ImproveService improveService;
-
 
     /**
      * 获取榜单列表
@@ -220,7 +214,6 @@ public class RankApiController {
     @ResponseBody
     @RequestMapping(value = "updatesymbol")
     public BaseResp<Object> updateRankSymbol(@RequestBody Rank rank){
-
         boolean issuccess = false;
         try {
             issuccess = rankService.updateRankSymbol(rank);
@@ -322,6 +315,24 @@ public class RankApiController {
             baseResp = rankService.hasPrivilege(rankid);
         } catch (Exception e) {
             logger.error("has privilege of rank id={} is error:{}",rankid,e);
+        }
+        return baseResp;
+    }
+
+    /**
+     * 查询pc榜单参榜最大人数-权限
+     * @param userid
+     * @param ranktype
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "maxjoinnum")
+    public BaseResp maxRankJoinNum(String userid,String ranktype){
+        BaseResp baseResp = new BaseResp();
+        try {
+            baseResp = rankService.maxRankJoinNum(userid,ranktype);
+        } catch (Exception e) {
+            logger.error("maxRankJoinNum userid={} ranktype={} is error:[}",userid,ranktype,e);
         }
         return baseResp;
     }
