bfaaaf6943b412079fa750e9781490bb5d2e01b7
wucanying <962933013@qq.com>
2017-06-15 17:09:51 +0800

    no message

--- src/main/java/com/longbei/appservice/controller/api/BehaviorRuleApiController.java
+++ src/main/java/com/longbei/appservice/controller/api/BehaviorRuleApiController.java
@@ -1,9 +1,12 @@
 package com.longbei.appservice.controller.api;
 
 import com.longbei.appservice.common.BaseResp;
+import com.longbei.appservice.common.Page;
 import com.longbei.appservice.common.constant.Constant;
 import com.longbei.appservice.entity.BehaviorRule;
+import com.longbei.appservice.entity.UserLevel;
 import com.longbei.appservice.service.BehaviorRuleService;
+import com.longbei.appservice.service.UserLevelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +30,8 @@ public class BehaviorRuleApiController {
 
     @Autowired
     private BehaviorRuleService behaviorRuleService;
-
+    @Autowired
+    private UserLevelService userLevelService;
 
     /**
      * 详情
@@ -45,7 +49,6 @@ public class BehaviorRuleApiController {
         }
         return baseResp;
     }
-
 
     /**
      * 编辑
@@ -85,9 +88,6 @@ public class BehaviorRuleApiController {
         return baseResp;
     }
 
-
-
-
     /**
      * 删除
      * @param id
@@ -110,5 +110,34 @@ public class BehaviorRuleApiController {
         return baseResp;
 
     }
+
+    /**
+     * @Description: 查看用户等级列表
+     * @param startNum 分页起始值
+     * @param pageSize 每页显示条数
+     * @auther IngaWu
+     * @currentdate:2017年6月1日
+     */
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "selectUserLevelList")
+    public BaseResp<Page<UserLevel>> selectUserLevelList(Integer startNum, Integer pageSize){
+        logger.info("selectUserLevelList for adminservice startNum={},pageSize={}",startNum,pageSize);
+        BaseResp<Page<UserLevel>> baseResp = new BaseResp<>();
+        if (null == startNum) {
+            startNum = 0;
+        }
+        if (null == pageSize) {
+            pageSize = 50;
+        }
+        try {
+            Page<UserLevel> page = userLevelService.selectUserLevelList(startNum,pageSize);
+            baseResp = BaseResp.ok();
+            baseResp.setData(page);
+        } catch (NumberFormatException e) {
+            logger.error("selectUserLevelList for adminservice startNum={},pageSize={}", startNum,pageSize,e);
+        }
+        return baseResp;
+    }
+
 
 }
