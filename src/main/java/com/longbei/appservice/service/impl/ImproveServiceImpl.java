68a590d2a408494ed6fbe4b5babb11318f9a8142
wucanying <962933013@qq.com>
2017-06-27 14:01:43 +0800

    no message

--- src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
@@ -1125,7 +1125,7 @@ public class ImproveServiceImpl implements ImproveService{
     //删除已结束的榜进步之后清理脏数据  不更新榜中排名
     private void clearFinishedRankDirtyData(Improve improve){
         int flower = improve.getFlowers();
-        int like = improve.getLikes();
+        int like = getLikeFromRedis(improve.getImpid().toString(),improve.getBusinessid().toString(),improve.getBusinesstype());
         String tableName = getTableNameByBusinessType(improve.getBusinesstype());
         String sourceTableName = getSourecTableNameByBusinessType(improve.getBusinesstype());
         if(improve.getIsmainimp().equals("1")){
