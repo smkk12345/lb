19800ccdc2305b822b28b288c5a8e3d7c5a493d9
lichaochao <1510138834@qq.com>
2017-07-05 17:30:08 +0800

    no message

--- src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
+++ src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
@@ -7,7 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("productService")
+@FeignClient("productServiceLcc")
 @RequestMapping("productService")
 public interface IProductCategoryService {
 	/**
--- src/main/java/com/longbei/appservice/service/api/userservice/IUserBasicService.java
+++ src/main/java/com/longbei/appservice/service/api/userservice/IUserBasicService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("userService")
+@FeignClient("userServiceLcc")
 @RequestMapping("userService")
 public interface IUserBasicService {
 	/**
