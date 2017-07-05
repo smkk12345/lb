df9d882e74b4a4d78e56e2b2ea23981cc195545c
lichaochao <1510138834@qq.com>
2017-07-05 17:31:52 +0800

    no message

--- src/main/java/com/longbei/appservice/service/api/productservice/IProductBasicService.java
+++ src/main/java/com/longbei/appservice/service/api/productservice/IProductBasicService.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("productServiceLcc")
+@FeignClient("productService")
 @RequestMapping("productService")
 public interface IProductBasicService {
 
--- src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
+++ src/main/java/com/longbei/appservice/service/api/productservice/IProductCategoryService.java
@@ -7,7 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("productServiceLcc")
+@FeignClient("productService")
 @RequestMapping("productService")
 public interface IProductCategoryService {
 	/**
