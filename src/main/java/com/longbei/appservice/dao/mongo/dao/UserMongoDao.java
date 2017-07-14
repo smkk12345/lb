d8e8de0fd78d5cf9390f35490d09a69cce129a16
wucanying <962933013@qq.com>
2017-07-14 15:22:28 +0800

    no message

--- src/main/java/com/longbei/appservice/dao/mongo/dao/UserMongoDao.java
+++ src/main/java/com/longbei/appservice/dao/mongo/dao/UserMongoDao.java
@@ -67,24 +67,26 @@ public class UserMongoDao extends BaseMongoDao<AppUserMongoEntity> {
 		if(!StringUtils.isBlank(user.getSex())){
 			update.set("sex", user.getSex());
 		}
-		if(!StringUtils.isBlank(user.getVcertification())){
+		if(!StringUtils.isBlank(user.getVcertification())) {
 			update.set("vcertification", user.getVcertification());
-		if(!StringUtils.isBlank(user.getDeviceindex())){
-			update.set("deviceindex", user.getDeviceindex());
-		}
-		try {
-			mongoTemplate1.upsert(query, update, AppUserMongoEntity.class);
-			if(StringUtils.isNotEmpty(user.getNickname()) || StringUtils.isNotEmpty(user.getAvatar())){
-				//如果用户更改了用户昵称或者用户头像,则更新融云的用户昵称和头像
-				BaseResp<Object> rongyunUpdateResp = this.rongYunService.refreshUserInfo(user.getUserid().toString(),user.getNickname(),user.getAvatar());
+			if (!StringUtils.isBlank(user.getDeviceindex())) {
+				update.set("deviceindex", user.getDeviceindex());
 			}
-		}catch (Exception e) {
-			logger.error("updateAppUserMongoEntity user = {}",
-					com.alibaba.fastjson.JSON.toJSON(user).toString(), e);
+			try {
+				mongoTemplate1.upsert(query, update, AppUserMongoEntity.class);
+				if (StringUtils.isNotEmpty(user.getNickname()) || StringUtils.isNotEmpty(user.getAvatar())) {
+					//如果用户更改了用户昵称或者用户头像,则更新融云的用户昵称和头像
+					BaseResp<Object> rongyunUpdateResp = this.rongYunService.refreshUserInfo(user.getUserid().toString(), user.getNickname(), user.getAvatar());
+				}
+			} catch (Exception e) {
+				logger.error("updateAppUserMongoEntity user = {}",
+						com.alibaba.fastjson.JSON.toJSON(user).toString(), e);
+			}
 		}
 		AppUserMongoEntity mongoUser =  getAppUser(user.getUserid().toString());
 		return mongoUser;
 	}
+
 	
 	public AppUserMongoEntity getAppUser(String userid){
 //		logger.info("getAppUser userid={}",userid);
