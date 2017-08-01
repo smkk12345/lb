bd19caf0e5860563d4cf4cf16354ce80c5b43473
wucanying <962933013@qq.com>
2017-08-01 17:23:58 +0800

    Supertopic

--- src/main/java/com/longbei/appservice/dao/SuperTopicMapper.java
+++ src/main/java/com/longbei/appservice/dao/SuperTopicMapper.java
@@ -6,21 +6,31 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface SuperTopicMapper {
-    int deleteByPrimaryKey(Integer id);
 
     int insert(SuperTopic record);
 
-    int insertSelective(SuperTopic record);
-
-    SuperTopic selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(SuperTopic record);
-
     int updateByPrimaryKey(SuperTopic record);
-
 
     List<SuperTopic> selectList(
                                 @Param("startNum")int startNum,
                                 @Param("endNum")int endNum);
+
+    List<SuperTopic> selectSuperTopicList(@Param("superTopic") SuperTopic SuperTopic,
+                                                @Param("startNum") Integer startNum,
+                                                @Param("pageSize") Integer pageSize);
+
+    int selectSuperTopicListCount(@Param("superTopic") SuperTopic SuperTopic);
+
+    SuperTopic selectSuperTopicByTopicId(@Param("topicId")Long topicId);
+
+    int removeSuperTopicByTopicId(@Param("topicId") Long topicId);
+
+    int insertSuperTopic(SuperTopic record);
+
+    int updateSuperTopicByTopicId(SuperTopic record);
+
+    int updateImpcount(@Param("topicId") Long topicId,@Param("num") Integer num);
+
+    int updateScancount(Long topicId);
 
 }
