9fe5596b319b8d3745906f2b61b2e37daba682aa
lixb <lixb@longbei.com>
2017-08-14 20:18:04 +0800

    no message

--- src/main/java/com/longbei/appservice/common/security/SecurityFilter.java
+++ src/main/java/com/longbei/appservice/common/security/SecurityFilter.java
@@ -63,7 +63,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 		if(urlPath.contains("/live/")){
 			String encrypt = request.getParameter("encrypt");
 			logger.info("urlPath time={}",encrypt);
-			encrypt.replaceAll("%252B","%2B");
+			encrypt = encrypt.replaceAll("%252B","%2B");
 			String params = AES.decrypt(AES.A_KEY,encrypt);
 			Map<String,Object> map = JSONObject.fromObject(params);
 			logger.info("urlPath map={}",JSONObject.fromObject(map).toString());
