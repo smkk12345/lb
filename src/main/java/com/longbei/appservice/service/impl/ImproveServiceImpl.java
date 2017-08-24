9300bdf386936bfc3b5e4999a77651a15ff74947
lixb <lixb@longbei.com>
2017-08-24 19:04:35 +0800

    优化推荐列表数据

--- src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
+++ src/main/java/com/longbei/appservice/service/impl/ImproveServiceImpl.java
@@ -3442,9 +3442,12 @@ public class ImproveServiceImpl implements ImproveService{
         try {
             List<TimeLineDetail> timeLineDetails = timeLineDetailDao.selectRecommendImproveList
                     (null,null,startno,pagesize);
+            Long uid = Long.parseLong(userid);
+            Set<String> friendids = this.userRelationService.getFriendIds(uid);
+            Set<String> fansIds = this.userRelationService.getFansIds(uid);
             Map<String, String> map = userRelationService.selectFriendRemarkList(userid);
+            Set<String> userCollectImproveIds = this.getUserCollectImproveId(userid);
             if (null != timeLineDetails && timeLineDetails.size() != 0){
-                Set<String> improveIds = this.getUserCollectImproveId(userid);
                 for (int i = 0 ; i < timeLineDetails.size() ; i++){
                     TimeLineDetail timeLineDetail = timeLineDetails.get(i);
                     Improve improve = new Improve();
@@ -3462,18 +3465,22 @@ public class ImproveServiceImpl implements ImproveService{
                     if(map.containsKey(user.getId())){
                         user.setNickname(map.get(user.getId()));
                     }
-
                     improve.setAppUserMongoEntity(user);
+//                    if(!Constant.VISITOR_UID.equals(userid)){
+//                        initUserRelateInfo(Long.parseLong(userid),timeLineDetail.getUser());
+//                        improve.setAppUserMongoEntity(timeLineDetail.getUser());
+//                        initImproveInfo(improve,Long.parseLong(userid));
+//                        if(improveIds.contains(improve.getImpid().toString())){
+//                            improve.setHascollect("1");
+//                        }
+//                    }
                     if(!Constant.VISITOR_UID.equals(userid)){
-                        initUserRelateInfo(Long.parseLong(userid),timeLineDetail.getUser());
-                        improve.setAppUserMongoEntity(timeLineDetail.getUser());
-                        initImproveInfo(improve,Long.parseLong(userid));
-
-                        if(improveIds.contains(improve.getImpid().toString())){
+                        initUserRelateInfo(uid,timeLineDetail.getUser(),friendids,fansIds);
+                        initImproveInfo(improve,uid);
+                        if(userCollectImproveIds.contains(improve.getImpid().toString())){
                             improve.setHascollect("1");
                         }
                     }
-
                     //初始化 赞 花 数量
 //                    initImproveLikeAndFlower(improve);
                     improve.setFlowers(timeLineDetail.getFlowers());
@@ -3488,7 +3495,6 @@ public class ImproveServiceImpl implements ImproveService{
         } catch (Exception e) {
             logger.error("select recommend list is error:",e);
         }
-
         return baseResp;
     }
 
