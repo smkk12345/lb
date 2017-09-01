dfd821bcd2e7dd7713181303d2c56476bcfb2615
yinxc <yinxc@longbei.com>
2017-09-01 15:16:10 +0800

    no message

--- src/main/java/com/longbei/appservice/service/LiveInfoMongoService.java
+++ src/main/java/com/longbei/appservice/service/LiveInfoMongoService.java
@@ -1,5 +1,8 @@
 package com.longbei.appservice.service;
 
+import java.util.Date;
+import java.util.List;
+
 import com.longbei.appservice.entity.LiveInfo;
 
 public interface LiveInfoMongoService {
@@ -15,5 +18,26 @@ public interface LiveInfoMongoService {
 	 * 根据liveid查询
 	 */
 	LiveInfo selectLiveInfoByLiveid(long liveid);
+	
+	
+	/**
+	 * @author yinxc
+	 * 获取所有的直播信息
+	 * @param isdel 是否删除    0:未删除   1：删除
+	 */
+	List<LiveInfo> selectLiveInfoList(String isdel);
+	
+	/**
+	 * @author yinxc
+	 * 修改
+	 */
+	void updateLiveInfo(long classroomid, Long courseid, Date starttime, Date endtime);
+	
+	
+	/**
+	 * @author yinxc
+	 * 删除
+	 */
+	void deleteLiveInfo(long classroomid, Long courseid);
 	
 }
