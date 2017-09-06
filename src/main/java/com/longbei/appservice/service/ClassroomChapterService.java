17f0248be76ce6ac0820de23a70e761d8686d12d
yinxc <yinxc@longbei.com>
2017-09-06 11:44:36 +0800

    no message

--- src/main/java/com/longbei/appservice/service/ClassroomChapterService.java
+++ src/main/java/com/longbei/appservice/service/ClassroomChapterService.java
@@ -3,6 +3,7 @@ package com.longbei.appservice.service;
 import java.util.List;
 
 import com.longbei.appservice.common.BaseResp;
+import com.longbei.appservice.common.Page;
 import com.longbei.appservice.entity.ClassroomChapter;
 
 public interface ClassroomChapterService {
@@ -40,5 +41,32 @@ public interface ClassroomChapterService {
 	 * param sort 章节序号
 	 */
 	BaseResp<Object> updateSortByid(long classroomid, long chapterid, Integer sort);
+	
+	/**
+	 * @author yinxc
+	 * 修改教室章节是否显示
+	 * 2017年9月6日
+	 * param chapterid  章节id
+	 * param classroomid 教室id
+	 * param display 0 显示 1 隐藏
+	 */
+	BaseResp<Object> updateDisplayByid(long classroomid, long chapterid, String display);
+	
+	
+	
+	
+	
+	//-----------------------admin调用--------------------------
+	
+	
+	/**
+    * @Description: 获取教室章节列表
+    * @param @param 正确返回 code 0 ，验证码不对，参数错误，未知错误返回相应状态码
+    * @auther yinxc
+    * @currentdate:2017年9月6日
+	*/
+	BaseResp<Page<ClassroomChapter>> selectPcSearchChapterList(ClassroomChapter classroomChapter, 
+			int startNo, int pageSize);
+		
 
 }
