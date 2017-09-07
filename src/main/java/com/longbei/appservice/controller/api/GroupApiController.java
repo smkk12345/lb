dc5aaed8358b91173bc009d747d63e5e754b34ba
wangyongzhi <wangyongzhi@longbei.com>
2017-09-07 14:38:10 +0800

    no message

--- src/main/java/com/longbei/appservice/controller/api/GroupApiController.java
+++ src/main/java/com/longbei/appservice/controller/api/GroupApiController.java
@@ -37,4 +37,22 @@ public class GroupApiController {
         baseResp = this.groupService.batchCreateGroup(mainGroupUserid,gradeid,groupname);
         return baseResp;
     }
+
+    /**
+     * 加入群组
+     * @param userid
+     * @param groupid
+     * @param groupuserid 群主id 当groupid群满了的时候,新建一个群,群主id即是groupuserid
+     * @param groupname 群名称 当groupid群满了的时候,新建一个群,群名称即是groupname
+     * @return
+     */
+    @RequestMapping(value="joinYSTInstitutionGroup")
+    public BaseResp<Object> joinYSTInstitutionGroup(Long userid,Long groupid,Long groupuserid,String groupname){
+        BaseResp<Object> baseResp = new BaseResp<Object>();
+        if(userid == null || groupid == null){
+            return baseResp.initCodeAndDesp(Constant.STATUS_SYS_07,Constant.RTNINFO_SYS_07);
+        }
+        baseResp = this.groupService.joinYSTInstitutionGroup(userid,groupid,groupuserid,groupname);
+        return baseResp;
+    }
 }
