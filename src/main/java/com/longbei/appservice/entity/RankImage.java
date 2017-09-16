b83c434a8aaca6658b505a4f14d04020a60832b7
yinxc <yinxc@longbei.com>
2017-09-16 17:12:20 +0800

    no message

--- src/main/java/com/longbei/appservice/entity/RankImage.java
+++ src/main/java/com/longbei/appservice/entity/RankImage.java
@@ -72,6 +72,10 @@ public class RankImage implements Serializable {
     private Integer diamondscore;//钻石对应排名分数
 
     private String codeword;//口令
+    
+    private Long audiotime;//音频长度（单位秒）
+
+    private Long videotime;//视频长度（单位秒）
 
 
     private String ptype;//十全十美类型
@@ -843,4 +847,20 @@ public class RankImage implements Serializable {
     public void setPtype(String ptype) {
         this.ptype = ptype == null ? null : ptype.trim();
     }
+
+	public Long getAudiotime() {
+		return audiotime;
+	}
+
+	public void setAudiotime(Long audiotime) {
+		this.audiotime = audiotime;
+	}
+
+	public Long getVideotime() {
+		return videotime;
+	}
+
+	public void setVideotime(Long videotime) {
+		this.videotime = videotime;
+	}
 }
