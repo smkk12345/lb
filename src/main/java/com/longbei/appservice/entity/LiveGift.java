6e5bee9e0bf763a807a9e370c5ca4a78724405d7
wucanying <962933013@qq.com>
2017-09-28 14:59:17 +0800

    礼物排序

--- src/main/java/com/longbei/appservice/entity/LiveGift.java
+++ src/main/java/com/longbei/appservice/entity/LiveGift.java
@@ -46,7 +46,7 @@ public class LiveGift implements Serializable {
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     public Date getUpdatetime() {
-        return null;
+        return updatetime;
     }
 
     public void setUpdatetime(Date updatetime) {
@@ -56,7 +56,7 @@ public class LiveGift implements Serializable {
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     public Date getCreatetime() {
-        return null;
+        return createtime;
     }
 
     public void setCreatetime(Date createtime) {
